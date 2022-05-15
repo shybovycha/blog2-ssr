67cfc8fb5ea81fdd7f04fe974cbc94e5444e1043
Artem Shoobovych <shybovycha@gmail.com>
2022-05-15 18:12:15 +1000

    Clean gh_pages branch

--- src/components/Home.svelte
+++
@@ -1,9 +0,0 @@
-{#each posts as { title, timestamp, excerpt, content, link }}
-    <Post title={title} timestamp={timestamp} excerpt={excerpt} content={content} link={link} isPreview />
-{/each}
-
-<script>
-	import Post from './Post.svelte';
-
-    export let posts = [];
-</script>
