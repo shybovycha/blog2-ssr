e1ecf64372c89e9a50b6d7eb121034e07bbc73b6
Artem Shubovych <shybovycha@gmail.com>
2022-07-25 17:52:30 +1000

    Enforce last updated at meta tag

--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="x-last-updated-at" content="{new Date().getTime()}">
 
     <link rel="icon" href="{baseUrl}/images/favicon-compressed.webp">
     <link rel="shortcut icon" href="{baseUrl}/images/favicon-compressed.webp">
